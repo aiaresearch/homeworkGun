2a565c344ad741e2a59170eb56811cadd19ac17a
Jim <3qiu.desu@gmail.com>
2024-02-15 16:02:50 +0800

    enabled all codes

--- main.py
+++ main.py
@@ -7,17 +7,17 @@ import keyboard
 
 if __name__ == '__main__':
     
-    while True:
-        # TODO: 当扳机and按键被按下时，调用cap.capImage()函数
-        if cv2.waitKey(1) & 0xFF == ord('c'):
+    # TODO: 当扳机按键被按下时，调用cap.capImage()函数
+    def on_key_press(event):
+        if event.name == 'c':
             print("Capturing image...")
             results = recognize(cap.capImage())
             print(results)
-        #     results = list(set(results))
+            results = list(set(results))
+            insert(results)
+            print(f"Successfully inserted {len(results)} submitted records.")
+            
 
-        #     insert(results)
-        #     print(f"Successfully inserted {len(results)} submitted records.")
-        
-        # if (cv2.waitKey(1) & 0xFF) == ord('q'):
-        #     break
-    
+    keyboard.on_press(on_key_press)
+    keyboard.wait('esc')
+    exit()
