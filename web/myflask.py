76b07fcb3ac87249b608ef14fe3b440ecd5d367e
3q1u <3qiu.desu@gmail.com>
2024-02-16 11:24:02 +0800

    Updated web parts

--- web/myflask.py
+++ web/myflask.py
@@ -34,12 +34,31 @@ def get_users():
     conn.close()
     return users
 
+# 从数据库中获取提交列表
+def get_submits():
+    conn = connect_to_db()
+    cur = conn.cursor()
+    cur.execute("SELECT id FROM submit;")
+    submits = cur.fetchall()
+    cur.close()
+    conn.close()
+    return submits
+
+def get_unsubmits():
+    conn = connect_to_db()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM students WHERE id NOT IN (SELECT id FROM submit);")
+    unsubmits = cur.fetchall()
+    cur.close()
+    conn.close()  
+    return unsubmits
+
 @app.route('/')
 def index():
     users = get_users()
-
-
-    return render_template('index.html', users=users)
+    submits = get_submits()  # 调用get_submits函数从数据库检索提交
+    unsubmits = get_unsubmits()  # 调用get_unsubmits函数从数据库检索未提交
+    return render_template('index.html', users=users, submits=submits, unsubmits=unsubmits)
 
 if __name__ == '__main__':
     app.run(debug=True)
