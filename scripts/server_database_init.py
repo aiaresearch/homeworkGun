3a8db432320d5c9bb3000bf667fc167ef17328c8
Jim <3qiu.desu@gmail.com>
2024-03-09 23:56:51 +0800

    Add user login and password for client

--- scripts/server_database_init.py
+++ scripts/server_database_init.py
@@ -14,41 +14,8 @@ def init_database():
     conn = get_connection(DatabaseType.POSTGRES)
     
     cursor = conn.cursor()
-    
-    # create tables
-    cursor.execute("""CREATE EXTENSION IF NOT EXISTS "uuid-ossp";""")
-    cursor.execute("""CREATE TABLE IF NOT EXISTS students (
-        id INTEGER PRIMARY KEY,
-        name VARCHAR(50) NOT NULL,
-        class_id INTEGER NOT NULL
-    );""")
-
-    cursor.execute("""CREATE TABLE IF NOT EXISTS classes (
-        id INTEGER PRIMARY KEY
-    );""")
-
-    cursor.execute("""CREATE TABLE IF NOT EXISTS homeworks (
-        id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-        title VARCHAR(50) NOT NULL,
-        description TEXT,
-        assigned_at DATE NOT NULL,
-        due_at DATE NOT NULL,
-        class_id INTEGER NOT NULL,
-                   
-        FOREIGN KEY (class_id) REFERENCES classes(id)
-    );""")
-
-    cursor.execute("""CREATE TABLE IF NOT EXISTS submissions (
-                   
-        id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-        homework_id UUID NOT NULL,
-                   
-        student_id INTEGER NOT NULL,
-        submitted_at DATE NOT NULL,
-                   
-        FOREIGN KEY (homework_id) REFERENCES homeworks(id),
-        FOREIGN KEY (student_id) REFERENCES students(id)
-    );""")
+    with open(os.path.join(os.path.abspath(os.path.dirname(os.pardir)), 'util', 'database', 'init_server_db.sql'), 'r') as file:
+        cursor.execute(file.read())
 
     conn.commit()
     conn.close()
--- util/query/get_data.py
+++ util/query/get_data.py
@@ -1,5 +1,7 @@
 import sys
 import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir, os.path.pardir)))
+from util.convert_passwd import convert
 
 
 def __get_student(conn, class_id):
@@ -8,3 +10,10 @@ def __get_student(conn, class_id):
     students = cur.fetchall()
     cur.close()
     return students
+
+def __match_user(conn, username, passwd):
+    cur = conn.cursor()
+    print(username, passwd)
+    cur.execute(f"SELECT password, salt FROM users WHERE username = '{username}';")
+    password, salt = cur.fetchone()
+    return True if convert(passwd, salt) == password else False
