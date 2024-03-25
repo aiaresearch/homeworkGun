99dd0f3ad2fb0e29ddecd278f28b1251a52d89bc
Deng Junhai <DengJunHai@foxmail.com>
2024-03-25 18:18:59 +0800

    Add CORS Rules
    Add CORS Rules

--- go_api/main.go
+++ go_api/main.go
@@ -2,21 +2,40 @@ package main
 
 import (
 	"homeworkGun/db_operation"
-
 	"homeworkGun/handlers"
 	"homeworkGun/middleware"
 	"log"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
-
 	_ "github.com/lib/pq"
 )
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(http.StatusNoContent)
+			return
+		}
+
+		c.Next()
+	}
+}
 
 func main() {
 	check_config()
 
 	r := gin.Default()
 	db := db_operation.ConnectToDB()
+
+	// Register the CORS middleware globally
+	r.Use(CORSMiddleware())
+
 	r.Use(func(c *gin.Context) {
 		db_operation.SetDBToContext(c, db)
 		c.Next()
